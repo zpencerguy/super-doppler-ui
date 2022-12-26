b6e2106193f225a4158f8d075a774c22af6d7479
Nathan Guy <nathanguy@Nathans-MacBook-Pro-3.local>
2022-12-26 15:42:42 -0600

    fixing values

--- pages/create.tsx
+++ pages/create.tsx
@@ -37,27 +37,27 @@ const Draft: React.FC = () => {
           /> */}
           <select onChange={(e) => setTitle(e.target.value)}>
             <option value="" disabled selected>Select Collection</option>
-            <option value="degods">DeGods</option>
-            <option value="okay_bears">Okay Bears</option>
-            <option value="y00ts">Y00ts</option>
-            <option value="degenerate_ape_academy">Degenerate Ape Academy</option>
-            <option value="solana_monkey_business">Solana Monkey Business</option>
-            <option value="t00bs">Y00ts mint t00bs</option>
-            <option value="abc_abracadabra">ABC</option>
-            <option value="claynosaurz">Claynosaurz</option>
-            <option value="trippin_ape_tribe">Trippin Ape Tribe</option>
-            <option value="cets_on_creck">Cets On Creck</option>
-            <option value="blocksmith_labs">Smyths</option>
-            <option value="galactic_geckos">GGSG Galactic Geckos</option>
-            <option value="shadowy_super_coder_dao">Shadowy Super Coder DAO</option>
-            <option value="primates">Primates</option>
-            <option value="famous_fox_federation">Famous Fox Federation</option>
-            <option value="aurory">Aurory</option>
-            <option value="lily">LILY</option>
-            <option value="portals">Portals</option>
-            <option value="communi3">Communi3 Mad Scientists</option>
-            <option value="beep_boop_battery">Beep Boop Battery</option>
-            <option value="the_catalina_whale_mixer">The Catalina Whale Mixer</option>
+            <option value="DeGods">DeGods</option>
+            <option value="Okay Bears<">Okay Bears</option>
+            <option value="Y00ts">Y00ts</option>
+            <option value="Degenerate Ape Academy">Degenerate Ape Academy</option>
+            <option value="Solana Monkey Business">Solana Monkey Business</option>
+            <option value="Y00ts mint t00bs">Y00ts mint t00bs</option>
+            <option value="ABC">ABC</option>
+            <option value="Claynosaurz">Claynosaurz</option>
+            <option value="Trippin Ape Tribe">Trippin Ape Tribe</option>
+            <option value="Cets On Creck">Cets On Creck</option>
+            <option value="Smyths">Smyths</option>
+            <option value="GGSG Galactic Geckos">GGSG Galactic Geckos</option>
+            <option value="Shadowy Super Coder DAO">Shadowy Super Coder DAO</option>
+            <option value="Primates">Primates</option>
+            <option value="Famous Fox Federation">Famous Fox Federation</option>
+            <option value="Aurory">Aurory</option>
+            <option value="LILY">LILY</option>
+            <option value="Portals">Portals</option>
+            <option value="Communi3 Mad Scientists">Communi3 Mad Scientists</option>
+            <option value="Beep Boop Battery">Beep Boop Battery</option>
+            <option value="The Catalina Whale Mixer">The Catalina Whale Mixer</option>
           </select>
           <textarea
             cols={50}

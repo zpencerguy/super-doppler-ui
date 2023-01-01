e4f94c3777ec9648a700b987204f713c7ad79115
Nathan Guy <nathanguy@Nathans-MBP-3.attlocal.net>
2023-01-01 15:33:18 -0600

    enabling mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
 // _app.tsx
-
+import * as React from 'react';
+import Head from 'next/head';
 import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from '../config/theme';
+import createEmotionCache from '../config/createEmotionCache';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (

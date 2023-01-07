9bd8ba74ca971fd44a5bafc3a0a29e1b4f2df844
Nathan Guy <nathanguy@Nathans-MacBook-Pro-3.local>
2023-01-06 23:49:40 -0600

    adding in date shit

--- components/Predictions.tsx
+++ components/Predictions.tsx
@@ -17,13 +17,26 @@ export type PredictionProps = {
 
 const Prediction: React.FC<{ collection: PredictionProps }> = ({ collection }) => {
   const name = collection.name;
-  const date = collection.date.toLocaleString();
+  //const date = collection.date.toLocaleString();
+
+  const date = new Date(collection.date.toLocaleString());
+  console.log(date)
+
+  const day = date.getDate();
+  const monthIndex = date.getMonth();
+  const year = date.getFullYear();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  const myFormattedDate = day+"-"+(monthIndex+1)+"-"+year+" "+ hours+":"+minutes+":"+seconds;
 
   return (
     <div>
-      <h2>{collection.name} - {collection.direction} - {date}</h2>
+      <h2>{collection.name}</h2>
+      <h4>Predicted direction: {collection.direction.toUpperCase()}</h4>
       <h4>Starting price: {collection.start_price} SOL</h4>
       <h4>Predicted price: {collection.end_price} SOL</h4>
+      <h4>Prediction made on: { myFormattedDate }</h4>
       <img
         className="rounded-lg"
         src={collection.imageurl}

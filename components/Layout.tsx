0a857e60b186635b0280f2d01d70d334dfb586b9
Nathan Guy <nathanguy@Nathans-MacBook-Pro-3.local>
2023-01-28 08:59:15 -0600

    adding background back in

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,8 +14,8 @@ const Layout: React.FC<Props> = (props) => (
     <style jsx global>{`
       html {
         box-sizing: border-box;
-        background: url(${bg.src});
-        background: transparent;
+        background-image: url(${bg.src});
+        background-color: transparent;
       }
 
       *,
@@ -31,6 +31,8 @@ const Layout: React.FC<Props> = (props) => (
         font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
           Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
           "Segoe UI Symbol";
+        background: transparent;
+        background-color: transparent;
       }
 
       input,
@@ -111,6 +113,8 @@ const Layout: React.FC<Props> = (props) => (
     <style jsx>{`
       .layout {
         padding: 0 2rem;
+        background: transparent;
+        background-color: transparent;
       }
     `}</style>
   </div>
